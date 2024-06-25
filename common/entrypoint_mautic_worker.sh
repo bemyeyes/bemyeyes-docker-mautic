5c08a187211271fdb8af8aa9f6bb6e6d89e3efc1
Ilkka Oksanen <iao@iki.fi>
2024-06-25 17:01:46 +0300

    Finalize plugin install also in worker

--- common/entrypoint_mautic_worker.sh
+++ common/entrypoint_mautic_worker.sh
@@ -6,4 +6,8 @@ until php -r 'file_exists("/var/www/html/config/local.php") ? include("/var/www/
 	sleep 5
 done
 
+# Needed to finish the AWS SES plugin installation (started in Dockerfile)
+php /var/www/html/bin/console cache:clear
+php /var/www/html/bin/console mautic:plugins:reload
+
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
